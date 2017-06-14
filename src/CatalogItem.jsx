24edb25da8da4e2ef18432698e985162a7ad6024
Pearl Zeng <zhuzeng@umich.edu>
2017-06-14 17:34:10 -0400

    Pull Catalog item out.

---
+++ src/CatalogItem.jsx
@@ -0,0 +1,58 @@
+/**
+ * @providesModule CatalogItem
+ * @flow
+ */
+
+import React, { Component } from 'react'
+
+import type { Product } from './schema'
+
+class CatalogItem extends Component {
+  props: Product & {
+    onSubmit: (id: string, quantity: number) => void,
+  }
+
+  state: { quantity: number } = {
+    quantity: 1,
+  }
+
+  handleChange = (e: SyntheticInputEvent) => {
+    this.setState({ quantity: e.target.valueAsNumber })
+  }
+
+  handleSubmit = () => {
+    const { id, onSubmit } = this.props
+    const { quantity } = this.state
+
+    onSubmit(id, quantity)
+  }
+
+  render () {
+    const { name, price } = this.props
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>${price}</td>
+        <td>
+          <select value={this.state.quantity} onBlur={this.handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <button type="submit" onClick={this.handleSubmit}>
+            Add to cart
+          </button>
+        </td>
+      </tr>
+    )
+  }
+}
+
+export default CatalogItem
