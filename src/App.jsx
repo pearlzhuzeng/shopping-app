021c9f1418fceec8127438195e9b5e68e6ff82f9
Pearl Zeng <zhuzeng@umich.edu>
2017-06-14 09:04:05 -0400

    Displays products properly.

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,36 @@ import styled from 'styled-components'
 import './App.css'
 
 const products = [
-  ('computer': {
+  {
     id: 'computer',
     name: 'computer',
     price: 2000,
-  }),
-  ('mouse': {
+  },
+  {
     id: 'mouse',
     name: 'mouse',
     price: 80,
-  }),
-  ('trackpad': {
+  },
+  {
     id: 'trackpad',
     name: 'trackpad',
     price: 130,
-  }),
-  ('keyboard': {
+  },
+  {
     id: 'keyboard',
     name: 'keyboard',
     price: 100,
-  }),
-  ('bag': {
+  },
+  {
     id: 'bag',
     name: 'bag',
     price: 100,
-  }),
-  ('charger': {
+  },
+  {
     id: 'charger',
     name: 'charger',
     price: 30,
-  }),
+  },
 ]
 
 type Item = {
@@ -113,7 +113,7 @@ class CatalogItem extends Component {
     return (
       <tr>
         <td>{name}</td>
-        <td>{price}</td>
+        <td>${price}</td>
       </tr>
     )
   }
