4bfeaf693822c658e682e58d6ba7ae57be8bd1bd
Pearl Zeng <zhuzeng@umich.edu>
2017-06-14 17:33:19 -0400

    Writes schema/date structure/types.

---
+++ src/schema.js
@@ -0,0 +1,22 @@
+/**
+ * @flow
+ */
+
+export type Product = {
+  id: string,
+  name: string,
+  price: number,
+}
+
+export type Catalog = {
+  [id: string]: Product,
+}
+
+export type Selection = {
+  productId: string,
+  quantity: number,
+}
+
+export type Cart = {
+  [productId: string]: Selection,
+}
