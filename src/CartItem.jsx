d21f2d3d7f8623b7e666c0fb8fc226f4bacf9b85
Pearl Zeng <zhuzeng@umich.edu>
2017-06-14 22:52:30 -0400

    Total price update based on quantity change in cart.

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,22 +3,46 @@
  * @flow
  */
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import type { Product, Selection } from './schema'
 
 type Props = Product & Selection
 
-const CartItem = (props: Props) => {
-  const { name, price, quantity } = props
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{price}</td>
-      <td>{quantity}</td>
-      <td>{price * quantity}</td>
-    </tr>
-  )
+class CartItem extends Component {
+  props: Props
+  state = {
+    quantity: this.props.quantity,
+  }
+
+  handleChange = (e: SyntheticInputEvent) => {
+    this.setState({ quantity: parseInt(e.target.value) })
+  }
+
+  render () {
+    const { name, price } = this.props
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>${price}</td>
+        <td>
+          <select value={this.state.quantity} onChange={this.handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </td>
+        <td>${price * this.state.quantity}</td>
+      </tr>
+    )
+  }
 }
 
 export default CartItem
