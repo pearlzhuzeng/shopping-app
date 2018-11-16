f4985647ad71408117071816fc2495213072a3e8
Pearl Zeng <zhuzeng@umich.edu>
2018-11-16 12:05:14 -0500

    Remove unused service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
 import products from './products.js'
 
 ReactDOM.render(<App products={products} />, document.getElementById('root'))
-registerServiceWorker()
